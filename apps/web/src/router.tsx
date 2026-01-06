8d9a57e04383b891082354fe96aeaf72fb726cae
Kevin Garcia <ermianrazor@gmail.com>
2026-01-06 14:28:07 -0500

    first commit

---
+++ apps/web/src/router.tsx
@@ -0,0 +1,21 @@
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+
+import Loader from "./components/loader";
+import { routeTree } from "./routeTree.gen";
+
+export const getRouter = () => {
+  const router = createTanStackRouter({
+    routeTree,
+    scrollRestoration: true,
+    defaultPendingComponent: () => <Loader />,
+    defaultNotFoundComponent: () => <div>Not Found</div>,
+    Wrap: ({ children }) => <>{children}</>,
+  });
+  return router;
+};
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
+}
---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,45 @@
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+import { Toaster } from "@/components/ui/sonner";
+import appCss from "@/index.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "My App",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
+
+  component: RootDocument,
+});
+
+function RootDocument() {
+  return (
+    <html className="dark" lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <Toaster richColors />
+        <TanStackRouterDevtools position="bottom-left" />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
